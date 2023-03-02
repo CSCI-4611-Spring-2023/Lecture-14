5ea3f6eb85ec84bb1789f080a51ef45034ab1842
Evan Suma Rosenberg <suma@umn.edu>
2023-03-02 16:37:37 -0600

    class update

--- src/MeshViewer.ts
+++ src/MeshViewer.ts
@@ -11,20 +11,22 @@ export class MeshViewer extends gfx.GfxApp
     private cameraControls: gfx.OrbitControls;
     private skybox: gfx.Mesh;
     private axes: gfx.Axes3;
+    private terrain: gfx.MorphMesh;
 
     constructor()
     {
         super();
 
         this.cameraControls = new gfx.OrbitControls(this.camera);
-        this.skybox = gfx.MeshFactory.createBox(1000, 1000, 500);
+        this.skybox = gfx.MeshFactory.createBox(1000, 1000, 1000);
         this.axes = new gfx.Axes3(20);
+        this.terrain = new gfx.MorphMesh();
     }
 
     createScene(): void 
     {
         // Setup camera
-        this.camera.setPerspectiveCamera(60, 1920/1080, 1, 1000)
+        this.camera.setPerspectiveCamera(60, 1920/1080, 1, 1500)
         this.cameraControls.setOrbit(-Math.PI/4, 0);
         this.cameraControls.setDistance(230);
         
@@ -46,6 +48,43 @@ export class MeshViewer extends gfx.GfxApp
         // Add the axes to the scene
         this.axes.position.set(0, 0.1, 0);
         this.scene.add(this.axes);
+
+        this.generateTerrain(300, 200, 4, 3);
+        this.scene.add(this.terrain);
+    }
+
+    private generateTerrain(width: number, depth: number, cols: number, rows: number): void
+    {
+        const vertices: gfx.Vector3[] = [];
+        const normals: gfx.Vector3[] = [];
+        const indices: number[] = [];
+
+        // Compute vertices and normals
+        for(let r=0; r < rows; r++)
+        {
+            for(let c=0; c < cols; c++)
+            {
+                vertices.push(new gfx.Vector3(r, 0, c));
+                normals.push(new gfx.Vector3(0, 1, 0));
+            }
+        }
+
+        // Compute the indices
+        for(let r=0; r < rows-1; r++)
+        {
+            for(let c=0; c < cols-1; c++)
+            {
+                const upperLeftIndex = 0;
+                const upperRightIndex = 0;
+                const lowerLeftIndex = 0;
+                const lowerRightIndex = 0;
+            }
+        }
+
+        this.terrain.setVertices(vertices);
+        this.terrain.setNormals(normals);
+        this.terrain.setIndices(indices);
+        this.terrain.createDefaultVertexColors();
     }
 
     update(deltaTime: number): void 
